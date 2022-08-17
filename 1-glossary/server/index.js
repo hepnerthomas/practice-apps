a260090f5ef1fc2938368699fdee3e7ad645c736
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-17 10:02:00 -0700

    feat: add local port to server app. install web-pack for dev-dependencies in package.json.

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -2,17 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 
+// set up port
+process.env.PORT = process.env.PORT || 3000;
+
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
 
 app.listen(process.env.PORT);
